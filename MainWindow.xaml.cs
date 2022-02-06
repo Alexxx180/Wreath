5a7a5472c00555e3a700add409901320b109046d
Alexxx180 <alexx120702@gmail.com>
2022-02-06 15:06:48 +0300

    Alpha 1.19 | Close window bug fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,10 @@ namespace Wreath
             {
                 ActivateAdmin();
             }
+            else
+            {
+                Close();
+            }
         }
 
         private void ActivateAdmin()
